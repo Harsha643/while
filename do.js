3b97068a902eb9068ec7252969e48e2a31fb5e8f
Battu Harshavardhan <battuharsha6@gmail.com>
2024-12-06 02:18:04 +0530

    Add files via upload

---
+++ do.js
@@ -0,0 +1,165 @@
+var i = 0;
+do {
+    if (i === 2) break;
+    console.log(i);
+    i++;
+} while (i < 5);
+////////////////////////
+var i = 0;
+while (i < 2) {
+    console.log(i);
+    i += 2;
+}
+////////////////////////////////////
+var i = 0;
+for (var j = 0; j < 3; j++) {
+    console.log(i);
+    i++;
+}
+////////////////////////////
+
+var i = 2;
+for (i = 0; i < 3; i++) {
+    console.log(i);
+}
+////////////////////////////////////
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 3);
+//////////////////////
+for (var i = 0; i < 4; i++) {
+    if (i === 2) continue;
+    console.log(i);
+}
+//////////////////////////////////////
+var i = 0;
+while (i < 10) {
+    i++;
+    console.log(i)
+    if (i == 5) continue;
+    console.log(i)
+}
+//////////////////////////////////
+var i = 0;
+for (; i < 4;) {
+    i++;
+    console.log(i);
+}
+///////////////////////////////////
+var i = 3;
+while (i < 5) {
+    i++;
+    console.log(i);
+}
+////////////////////////////////////
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 2);
+////////////////////////////////////
+var i = 0;
+for (i = 0; i < 3; i++) {
+    console.log(i);
+}
+//////////////////////////////////
+var i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+//////////////////////////////
+var i = 0;
+while (i < 4) {
+    console.log(i++);
+}
+///////////////////////////
+var i = 0;
+for (var j = 0; j < 2; j++) {
+    i++;
+    console.log(i);
+}
+///////////////////////////
+var i = 1;
+for (i = 0; i < 10; i++) {
+    if (i == 5) break;
+    console.log(i)
+}
+////////////////////////////////
+for (var i = 0; i < 5; i++) {
+    if (i === 3) continue;
+    console.log(i);
+}
+////////////////////////////////
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 0);
+console.log(i);
+/////////////////////////////////
+var i = 0;
+for (var j = 0; j < 2; j++) {
+    i++;
+}
+console.log(i);
+//////////////////////////////////
+var i = 5;
+while (i <= 5) {
+    i++;
+    console.log(i)
+}
+//////////////////////////////////
+var i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i < 4);
+var i = 0;
+////////////////////////////////////
+for (; i < 3;) {
+    console.log(i);
+    i++;
+}
+/////////////////////////////////////
+var i = 3;
+while (i > 0) {
+    console.log(i);
+    i--;
+}
+///////////////////////////////////////
+var i = 0;
+do {
+    
+    i++;
+    console.log(i)
+} while (i < 10);
+//////////////////////////////////////
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 3);
+/////////////////////////////
+for (var i = 0; i <= 3; i++) {
+    console.log(i);
+}
+/////////////////////////////////////////
+var i = 0;
+while (i < 5) {
+    i++;
+    console.log(i)
+}
+///////////////////////////////////////
+var i = 0;
+while (i < 2) {
+    console.log(i);
+    i++;
+}
+///////////////////////////////////
+var i = 0;
+for (i = 0; i < 3; i++) {
+    console.log(i);
+}
